f2542d36d9312166468299abfd535b3bee9c4f69
Ali Eren <ali_erenbatman@hotmail.com>
2023-03-19 21:24:08 +0300

    Arranging files and environments

---
+++ slice-it-all-clone/Assets/Scripts/knifefront.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knifefront : MonoBehaviour
+{
+    public GameObject knifehandler;
+    public bool onair;
+    public bool triggertrigger;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            onair = false;
+            knifehandler.GetComponent<control>().speedy = knifehandler.GetComponent<control>().speedx = 0;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        onair = true;
+    }
+}
